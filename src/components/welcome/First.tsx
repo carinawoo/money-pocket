26b667238b0efe3ea5f5b6a0894f6c6efdc4ff7d
carinawoo <2770130836@qq.com>
2023-08-06 23:39:04 +0800

    进一步重构

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,22 +1,19 @@
-import { defineComponent } from 'vue';
 import s from './WelcomeLayout.module.scss';
 import exchange from '../../assets/icons/exchange.svg';
 import { RouterLink } from 'vue-router';
 import { WelcomeLayout } from './WelcomeLayout';
+import { FunctionalComponent } from 'vue';
 
-export const First = defineComponent({
-  setup: (props, context) => {
-    const slots = {
-      icon: () => <img src={exchange} />,
-      title: () => <h2>会挣钱<br />还会省钱</h2>,
-      buttons: () => <>
-        <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-        <RouterLink to="/welcome/2" >下一页</RouterLink>
-        <RouterLink to="/start" >跳过</RouterLink>
-      </>
-    }
-    return () => (
-      <WelcomeLayout v-slots={slots}></WelcomeLayout>
-    )
-  }
-})
+export const First: FunctionalComponent = () => {
+  return <WelcomeLayout>{{
+    icon: () => <img src={exchange} />,
+    title: () => <h2>会挣钱<br />还会省钱</h2>,
+    buttons: () => <>
+      <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+      <RouterLink to="/welcome/2" >下一页</RouterLink>
+      <RouterLink to="/start" >跳过</RouterLink>
+    </>
+  }}</WelcomeLayout>
+}
+
+First.displayName = 'First'
