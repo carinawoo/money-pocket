0127143c6d7a017accaea7f7bd0f06d8510cf953
carinawoo <2770130836@qq.com>
2023-08-06 23:27:45 +0800

    重构：提取 WelcomeLayout 组件

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,21 +1,22 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './WelcomeLayout.module.scss';
 import exchange from '../../assets/icons/exchange.svg';
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
+
 export const First = defineComponent({
   setup: (props, context) => {
+    const slots = {
+      icon: () => <img src={exchange} />,
+      title: () => <h2>会挣钱<br />还会省钱</h2>,
+      buttons: () => <>
+        <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+        <RouterLink to="/welcome/2" >下一页</RouterLink>
+        <RouterLink to="/start" >跳过</RouterLink>
+      </>
+    }
     return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img src={exchange} />
-          <h2>会挣钱<br/>还要会省钱</h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-          <RouterLink to="/welcome/2" >下一页</RouterLink>
-          <RouterLink to="/start" >跳过</RouterLink>
-        </div>
-      </div>
+      <WelcomeLayout v-slots={slots}></WelcomeLayout>
     )
   }
 })
