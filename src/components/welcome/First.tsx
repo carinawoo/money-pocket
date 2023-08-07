0a149329e062bd09dbb5fc509b81e8c588d596e1
carinawoo <2770130836@qq.com>
2023-08-07 18:02:35 +0800

    完成欢迎界面动画

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,19 +1,12 @@
-import s from './WelcomeLayout.module.scss';
+import s from './welcome.module.scss';
 import exchange from '../../assets/icons/exchange.svg';
-import { RouterLink } from 'vue-router';
-import { WelcomeLayout } from './WelcomeLayout';
 import { FunctionalComponent } from 'vue';
 
 export const First: FunctionalComponent = () => {
-  return <WelcomeLayout>{{
-    icon: () => <img src={exchange} />,
-    title: () => <h2>会挣钱<br />还会省钱</h2>,
-    buttons: () => <>
-      <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-      <RouterLink to="/welcome/2" >下一页</RouterLink>
-      <RouterLink to="/start" >跳过</RouterLink>
-    </>
-  }}</WelcomeLayout>
+  return <div class={s.card}>
+    <img src={exchange}/>
+    <h2>会挣钱<br />还会省钱</h2>
+  </div>
 }
 
 First.displayName = 'First'
