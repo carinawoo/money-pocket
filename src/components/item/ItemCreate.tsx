1a1084e98304f60762f30e3632846265c5fb3702
carinawoo <2770130836@qq.com>
2023-08-19 02:16:13 +0800

    封装后退按钮

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -3,6 +3,7 @@ import { Dialog } from 'vant';
 import { defineComponent, onMounted, PropType, reactive, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { MainLayout } from '../../layouts/MainLayout';
+import { BackIcon } from '../../shared/BackIcon';
 import { Button } from '../../shared/Button';
 import { http } from '../../shared/Http';
 import { Icon } from '../../shared/Icon';
@@ -43,7 +44,7 @@ export const ItemCreate = defineComponent({
     return () => (
       <MainLayout class={s.layout}>{{
         title: () => '记一笔',
-        icon: () => <Icon name="left" class={s.navIcon} />,
+        icon: () => <BackIcon />,
         default: () => <>
           <div class={s.wrapper}>
             <Tabs v-model:selected={formData.kind} class={s.tabs}>
