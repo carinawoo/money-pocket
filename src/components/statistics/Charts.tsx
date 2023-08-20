2ff91777b6fa950b89e6e9bca6b27603b042df94
carinawoo <2770130836@qq.com>
2023-08-21 03:14:43 +0800

    重构 betterData1

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -9,7 +9,7 @@ import { Time } from '../../shared/time';
 
 const DAY = 24 * 3600 * 1000
 
-type Data1Item = {happen_at:string, amount: number}
+type Data1Item = { happen_at: string; amount: number }
 type Data1 = Data1Item[]
 
 export const Charts = defineComponent({
@@ -27,27 +27,21 @@ export const Charts = defineComponent({
     const kind = ref('expenses')
     const data1 = ref<Data1>([])
     const betterData1 = computed<[string, number][]>(()=> {
-      if(!props.startDate || !props.endDate) {
-        return []
-      }
-      const array = []
+      if(!props.startDate || !props.endDate) { return [] }
       const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
       const n = diff / DAY + 1
-      let data1Index = 0
-      for(let i=0; i<n; i++) {
-        const time = new Time(props.startDate+'T00:00:00.000+0800').add(i, 'day').getTimestamp()
-        if(data1.value[data1Index] && new Date(data1.value[data1Index].happen_at).getTime() === time){
-          array.push([new Date(time).toISOString(), data1.value[data1Index].amount])
-          data1Index += 1
-        }else{
-          array.push([new Date(time).toISOString(), 0])
-        }
-      }
-      return array as [string, number][]
+      return Array.from({ length: n }).map((_, i) => {
+        const time = new Time(props.startDate + 'T00:00:00.000+0800').add(i, 'day').getTimestamp()
+        const item = data1.value[0]
+        const amount = (item && new Date(item.happen_at).getTime() === time)
+          ? data1.value.shift()!.amount
+          : 0
+        return [new Date(time).toISOString(), amount]
+      })
     })
 
-    onMounted(async ()=>{
-      const response = await http.get<{groups: Data1, summary: number}>('/items/summary',{
+    onMounted(async () => {
+      const response = await http.get<{ groups: Data1; summary: number }>('/items/summary', {
         happen_after: props.startDate,
         happen_before: props.endDate,
         kind: kind.value,
@@ -60,10 +54,15 @@ export const Charts = defineComponent({
 
     return () => (
       <div class={s.wrapper}>
-        <FormItem label='类型' type="select" options={[
-          { value: 'expenses', text: '支出' },
-          { value: 'income', text: '收入' }
-        ]} v-model={kind.value} />
+        <FormItem
+          label="类型"
+          type="select"
+          options={[
+            { value: 'expenses', text: '支出' },
+            { value: 'income', text: '收入' }
+          ]} 
+          v-model={kind.value}
+        />
         <LineChart data={betterData1.value}/>
         <PieChart />
         <Bars />
